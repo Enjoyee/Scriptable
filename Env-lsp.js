5b350e591f35d77f0481e4f48ff1353d7fa304da
Enjoyee <kwqjk03@163.com>
2020-12-12 11:57:31 +0800

    Update Env-lsp.js

--- Env-lsp.js
+++ Env-lsp.js
@@ -1,4 +1,5 @@
 // Variables used by Scriptable.
+// This script was created by Max Zeryck.
 
 /* -----------------------------------------------
 这是一份模板，可以导入进行使用里面封装的方法变量，谨慎修改!!
@@ -18,7 +19,7 @@ let configs = {
     leftPadding: 0, // 内容区边距
     bottomPadding: 0, // 内容区边距
     rightPadding: 0, // 内容区边距
-    refreshInterval: 0 // 刷新间隔，单位分钟，非精准，会有3-5分钟差距
+    refreshInterval: 1 // 刷新间隔，单位分钟，非精准，会有3-5分钟差距
 }
 exports.configs = configs
 //------------------------------------------------
@@ -77,8 +78,32 @@ exports.run = async function (scriptName, widget, autoDak = false, autoRun = tru
         subTips = `在${appearanceStr}下的`
     }
     const okTips = `您的小部件${subTips}背景已准备就绪，请退到桌面查看即可。`
-    let message = "图片模式支持相册照片&背景透明"
-    let options = ["图片选择", "透明背景"]
+
+
+    let message = "开始之前，请回到主屏幕并进入编辑模式。 滑到最右边的空白页并截图。"
+    let options = ["继续选择图片", "退出进行截图", "更新Env脚本"]
+    let response = await generateAlert(message, options)
+    if (response == 1) return
+    if (response == 2) {
+        let files = FileManager.local()
+        const iCloudInUse = files.isFileStoredIniCloud(module.filename)
+        files = iCloudInUse ? FileManager.iCloud() : files
+        try {
+            const req = new Request("https://gitee.com/enjoyee/scriptable/raw/master/Env-lsp.js")
+            const codeString = await req.loadString()
+            files.writeString(module.filename, codeString)
+            message = "Env脚本已更新，下次运行直接生效。"
+        } catch {
+            message = "更新失败，请稍后再试。"
+        }
+        options = ["好的"]
+        await generateAlert(message, options)
+        return
+    }
+
+
+    message = "支持相册照片&背景透明"
+    options = ["图片选择", "透明背景"]
     let isTransparentMode = await generateAlert(message, options)
     if (!isTransparentMode) {
         let img = await Photos.fromLibrary()
@@ -87,13 +112,6 @@ exports.run = async function (scriptName, widget, autoDak = false, autoRun = tru
         await generateAlert(message, resultOptions)
         fm.writeImage(path, img)
     } else {
-        message = "以下是【透明背景】生成步骤，如果你没有屏幕截图请退出，并返回主屏幕长按进入编辑模式。滑动到最右边的空白页截图，然后重新运行！"
-        let exitOptions = ["继续(已有截图)", "退出(没有截图)"]
-
-        let shouldExit = await generateAlert(message, exitOptions)
-        if (shouldExit) return
-
-        // Get screenshot and determine phone size.
         let img = await Photos.fromLibrary()
         let height = img.size.height
         let phone = phoneSizes()[height]
@@ -103,7 +121,23 @@ exports.run = async function (scriptName, widget, autoDak = false, autoRun = tru
             return
         }
 
-        // Prompt for widget size and position.
+        if (height == 2436) {
+            let files = FileManager.local()
+            let cacheName = "lsp-phone-type"
+            let cachePath = files.joinPath(files.libraryDirectory(), cacheName)
+            if (files.fileExists(cachePath)) {
+                let typeString = files.readString(cachePath)
+                phone = phone[typeString]
+            } else {
+                message = "你使用什么型号的iPhone？"
+                let types = ["iPhone 12 mini", "iPhone 11 Pro, XS, or X"]
+                let typeIndex = await generateAlert(message, types)
+                let type = (typeIndex == 0) ? "mini" : "x"
+                phone = phone[type]
+                files.writeString(cachePath, type)
+            }
+        }
+
         message = "您想要创建什么尺寸的小部件？"
         let sizes = ["小号", "中号", "大号"]
         let size = await generateAlert(message, sizes)
@@ -112,7 +146,6 @@ exports.run = async function (scriptName, widget, autoDak = false, autoRun = tru
         message = "您想它在什么位置？"
         message += (height == 1136 ? " (请注意，您的设备仅支持两行小部件，因此中间和底部选项相同。)" : "")
 
-        // Determine image crop based on phone size.
         let crop = { w: "", h: "", x: "", y: "" }
         if (widgetSize == "小号") {
             crop.w = phone.小号
@@ -120,7 +153,6 @@ exports.run = async function (scriptName, widget, autoDak = false, autoRun = tru
             let positions = ["顶部 左边", "顶部 右边", "中间 左边", "中间 右边", "底部 左边", "底部 右边"]
             let position = await generateAlert(message, positions)
 
-            // Convert the two words into two keys for the phone size dictionary.
             let keys = positions[position].split(' ')
             crop.y = phone[keys[0]]
             crop.x = phone[keys[1]]
@@ -129,7 +161,6 @@ exports.run = async function (scriptName, widget, autoDak = false, autoRun = tru
             crop.w = phone.中号
             crop.h = phone.小号
 
-            // 中号 and 大号 widgets have a fixed x-value.
             crop.x = phone.左边
             let positions = ["顶部", "中间", "底部"]
             let position = await generateAlert(message, positions)
@@ -143,17 +174,20 @@ exports.run = async function (scriptName, widget, autoDak = false, autoRun = tru
             let positions = ["顶部", "底部"]
             let position = await generateAlert(message, positions)
 
-            // 大号 widgets at the 底部 have the "中间" y-value.
             crop.y = position ? phone.中间 : phone.顶部
         }
 
-        // Crop image and finalize the widget.
         let imgCrop = cropImage(img, new Rect(crop.x, crop.y, crop.w, crop.h))
 
-        message = okTips
-        const resultOptions = ["好的"]
-        await generateAlert(message, resultOptions)
-        fm.writeImage(path, imgCrop)
+        message = `${okTips} 是否需要保存图像?`
+        const exportPhotoOptions = ["导出到相册", "直接完成"]
+        const exportToFiles = await generateAlert(message, exportPhotoOptions)
+
+        if (exportToFiles) {
+            fm.writeImage(path, imgCrop)
+        } else {
+            Photos.save(imgCrop)
+        }
     }
 
     if (autoRun) {
@@ -177,6 +211,7 @@ exports.readBgImg = function () {
 }
 //------------------------------------------------
 exports.finish = function (widget, preview = true) {
+    widget.refreshAfterDate = new Date(exports.configs.refreshInterval * 60 * 1000)
     // 设置边距(上，左，下，右)
     widget.setPadding(exports.configs.topPadding, exports.configs.leftPadding, exports.configs.bottomPadding, exports.configs.rightPadding)
     // 设置组件
@@ -193,7 +228,6 @@ exports.finish = function (widget, preview = true) {
             widget.presentSmall()
         }
     }
-
 }
 //------------------------------------------------
 // Generate an alert with the provided array of options.
@@ -220,40 +254,32 @@ function cropImage(img, rect) {
 // Pixel sizes and positions for widgets on all supported phones.
 function phoneSizes() {
     let phones = {
-        "2340": { // 12mini
-            "小号": 436,
-            "中号": 936,
-            "大号": 980,
-            "左边": 72,
-            "右边": 570,
-            "顶部": 212,
-            "中间": 756,
-            "底部": 1300,
-        },
-
-        "2532": { // 12/12 Pro
-            "小号": 472,
-            "中号": 1012,
-            "大号": 1058,
+        "2532": {
+            "models": ["12", "12 Pro"],
+            "小号": 474,
+            "中号": 1014,
+            "大号": 1062,
             "左边": 78,
             "右边": 618,
-            "顶部": 230,
-            "中间": 818,
-            "底部": 1408,
+            "顶部": 231,
+            "中间": 819,
+            "底部": 1407,
         },
 
-        "2778": { // 12 Pro Max
-            "小号": 518,
-            "中号": 1114,
-            "大号": 1162,
-            "左边": 86,
+        "2778": {
+            "models": ["12 Pro Max"],
+            "小号": 510,
+            "中号": 1092,
+            "大号": 1146,
+            "左边": 96,
             "右边": 678,
-            "顶部": 252,
-            "中间": 898,
-            "底部": 1544,
+            "顶部": 246,
+            "中间": 882,
+            "底部": 1518,
         },
 
         "2688": {
+            "models": ["Xs Max", "11 Pro Max"],
             "小号": 507,
             "中号": 1080,
             "大号": 1137,
@@ -265,6 +291,7 @@ function phoneSizes() {
         },
 
         "1792": {
+            "models": ["11", "Xr"],
             "小号": 338,
             "中号": 720,
             "大号": 758,
@@ -276,17 +303,33 @@ function phoneSizes() {
         },
 
         "2436": {
-            "小号": 465,
-            "中号": 987,
-            "大号": 1035,
-            "左边": 69,
-            "右边": 591,
-            "顶部": 213,
-            "中间": 783,
-            "底部": 1353
+            x: {
+                "models": ["X", "Xs", "11 Pro"],
+                "小号": 465,
+                "中号": 987,
+                "大号": 1035,
+                "左边": 69,
+                "右边": 591,
+                "顶部": 213,
+                "中间": 783,
+                "底部": 1353
+            },
+
+            mini: {
+                "models": ["12 mini"],
+                "小号": 465,
+                "中号": 987,
+                "大号": 1035,
+                "左边": 69,
+                "右边": 591,
+                "顶部": 231,
+                "中间": 801,
+                "底部": 1371
+            }
         },
 
         "2208": {
+            "models": ["6+", "6s+", "7+", "8+"],
             "小号": 471,
             "中号": 1044,
             "大号": 1071,
@@ -298,6 +341,7 @@ function phoneSizes() {
         },
 
         "1334": {
+            "models": ["6", "6s", "7", "8"],
             "小号": 296,
             "中号": 642,
             "大号": 648,
@@ -309,6 +353,7 @@ function phoneSizes() {
         },
 
         "1136": {
+            "models": ["5", "5s", "5c", "SE"],
             "小号": 282,
             "中号": 584,
             "大号": 622,
@@ -574,6 +619,66 @@ exports.getDateStr = function (date, formatter = "yyyy年MM月d日 EEE", locale 
     df.locale = locale
     df.dateFormat = formatter
     return df.string(date)
+}
+//------------------------------------------------
+/**
+ * 获取图片
+ * @param {string} url 图片链接
+ * @param {string} cacheKey 缓存的key
+ * @param {bool} useCache 是否使用缓存
+ * @return {Image}
+ */
+exports.getImageByUrl = async function (url, cacheKey, useCache = true) {
+    const cacheFile = FileManager.local().joinPath(FileManager.local().temporaryDirectory(), cacheKey)
+    const exists = FileManager.local().fileExists(cacheFile)
+    // 判断是否有缓存
+    if (useCache && exists) {
+        return Image.fromFile(cacheFile)
+    }
+    try {
+        const req = new Request(url)
+        const img = await req.loadImage()
+        // 存储到缓存
+        FileManager.local().writeImage(cacheFile, img)
+        return img
+    } catch (e) {
+        console.error(`图片加载失败：${e}`)
+        if (exists) {
+            return Image.fromFile(cacheFile)
+        }
+        // 没有缓存+失败情况下，返回黑色背景
+        let ctx = new DrawContext()
+        ctx.size = new Size(100, 100)
+        ctx.setFillColor(Color.black())
+        ctx.fillRect(new Rect(0, 0, 100, 100))
+        return await ctx.getImage()
+    }
+}
+//------------------------------------------------
+/**
+ * Http Get 请求接口
+ * @param {string} url 请求的url
+ * @param {string} cacheKey 缓存key
+ * @param {bool} json 返回数据是否为json，默认true
+ * @return {string | json | null}
+ */
+exports.httpGet = async function (url, cacheKey, json = true) {
+    let data = null
+    try {
+        let req = new Request(url)
+        data = await (json ? req.loadJSON() : req.loadString())
+    } catch (e) {
+        console.error(`httpGet请求失败：${e}`)
+    }
+    // 判断数据是否为空（加载失败）
+    if (!data && Keychain.contains(cacheKey)) {
+        // 判断是否有缓存
+        let cache = Keychain.get(cacheKey)
+        return json ? JSON.parse(cache) : cache
+    }
+    // 存储缓存
+    Keychain.set(cacheKey, json ? JSON.stringify(data) : data)
+    return data
 }
 //------------------------------------------------
 module.exports = exports
