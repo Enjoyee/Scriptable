280ff14873b80512a198552c4811d67ffc2abe28
enjoyeetime <kwqjk03@126.com>
2023-05-09 13:26:33 +0800

    优化缓存

--- 万年历.js
+++ 万年历.js
@@ -411,15 +411,27 @@ class Widget extends BaseWidget {
     const totalCalendarArr = [];
     //
     const currDate = new Date();
-    const url = `https://opendata.baidu.com/api.php?tn=wisetpl&format=json&resource_id=39043&query=${currDate.getFullYear()}%E5%B9%B4${currDate.getMonth() + 1}%E6%9C%88&t=${currDate.getTime()}`
-    const response = await this.httpGet(
-      url,
-      {
-        jsonFormat: false,
-      }
-    );
+    const dateKey = `${currDate.getFullYear()}-${currDate.getMonth() + 1}`;
+    const useCache = keyGet(dateKey, '0') == 1;
+    let response;
+    if (useCache) {
+      response = keyGet(this.md5(this.scriptName), '');
+      console.log(`使用缓存的${dateKey}日历数据...`);
+    }
+    if (response.length == 0) {
+      console.log(`网络请求日历数据...`);
+      const url = `https://opendata.baidu.com/api.php?tn=wisetpl&format=json&resource_id=39043&query=${currDate.getFullYear()}%E5%B9%B4${currDate.getMonth() + 1}%E6%9C%88&t=${currDate.getTime()}`
+      response = await this.httpGet(
+        url,
+        {
+          jsonFormat: false,
+        }
+      );
+    }
     const json = JSON.parse(response);
     if (json.status == 0) {
+      keySave(dateKey, '1');
+      keySave(this.md5(this.scriptName), response);
       const { data = [] } = json;
       if (data.length > 0) {
         const { almanac = [] } = data[0]
